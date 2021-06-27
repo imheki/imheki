49f89164b7423f765cd67371d1eb59a84147b579
H E K I <83841739+imheki@users.noreply.github.com>
2021-06-27 01:21:48 -0500

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,10 @@
+const Heki = {
+	name: 'Heki',
+	from: 'Mexico',
+	year: 16,
+	lenguage: 'spanish',
+	extra_information: {
+		principal_lenguage: 'JavaScript',
+		secundary_lenguage: 'Python',
+	}
+}
